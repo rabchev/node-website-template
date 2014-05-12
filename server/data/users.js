34d77eb90a92d9c76ec67b7bab12dc77c14e2eb9
Boyan Rabchev <boyan@rabchev.com>
2014-05-12 18:55:25 +0300

    Users improvements.

--- server/data/users.js
+++ server/data/users.js
@@ -1,6 +1,7 @@
-var users = [
-    {
-        _id: "1",
+var _       = require("lodash"),
+    users   = {
+    1: {
+        _id: 1,
         name: "Bob Pott",
         nickname: "bob",
         password: "secret",
@@ -10,8 +11,8 @@ var users = [
             "administrators"
         ]
     },
-    {
-        _id: "2",
+    2: {
+        _id: 2,
         name: "Joe Haxby",
         nickname: "joe",
         password: "birthday",
@@ -21,23 +22,72 @@ var users = [
             "contributers"
         ]
     }
-];
+};
 
-function findById(id, fn) {
-    var idx = id - 1;
-    if (users[idx]) {
-        fn(null, users[idx]);
-    } else {
-        fn(new Error("User " + id + " does not exist"));
-    }
-}
-
-function findByUsername(username, fn) {
-    for (var i = 0, len = users.length; i < len; i++) {
-        var user = users[i];
-        if (user.username === username || user.email === username) {
-            return fn(null, user);
+function returnBack(callback, err, res) {
+    // Simulate IO
+    setImmediate(function () {
+        if (callback) {
+            callback(err, res);
+        } else if (err) {
+            throw err;
         }
-    }
-    return fn(null, null);
+    });
 }
+
+function findById(id, callback) {
+    var user = users[id],
+        err;
+
+    if (user) {
+        user = _.cloneDeep(user);
+    } else {
+        err = new Error("User \"" + id + "\" does not exist.");
+    }
+    returnBack(callback, err, user);
+}
+
+function findByUsername(username, callback) {
+    var user = _.find(users, function (user) {
+        return (user.username === username || user.email === username);
+    });
+    if (user) {
+        user = _.cloneDeep(user);
+    }
+    returnBack(callback, null, user);
+}
+
+function update(user, callback) {
+    var dest = users[user._id],
+        err;
+
+    if (user) {
+        dest = _.cloneDeep(_.merge(dest, user));
+    } else {
+        err = new Error("User \"" + id + "\" does not exist.");
+    }
+
+    returnBack(callback, err, dest);
+}
+
+function insert(user, callback) {
+    var dest = users[user._id],
+        err;
+    if (dest) {
+        err = new Error("There is already a user with the specified ID.");
+    } else {
+        users[user._id] = user;
+    }
+    returnBack(callback, err, dest);
+}
+
+function deleteUser(id, callback) {
+    delete users[id];
+    returnBack(callback);
+}
+
+exports.findById = findById;
+exports.findByUsername = findByUsername;
+exports.update = update;
+exports.insert = insert;
+exports.delete = deleteUser;
